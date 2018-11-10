91a4527e7cd111c09630de24cfd61bc2c059a0f4
Gregory Higley <greg.higley@vendita.com>
2018-11-10 05:12:58 -0500

    ProcessLister works!

--- ProcessLister/MainWindow.xaml.vb
+++ ProcessLister/MainWindow.xaml.vb
@@ -1,3 +1,12 @@
-﻿Class MainWindow
+﻿Imports System.Configuration
+Imports Vendita.MAS
+
+Class MainWindow
+
+    Private Async Sub Window_Loaded(sender As Object, e As RoutedEventArgs)
+        Dim url As String = ConfigurationManager.AppSettings("Vendita.MAS.Server")
+        Dim api As IApi = New Api(url)
+        DataContext = Await api.ListProcessesAsync()
+    End Sub
 
 End Class
